ccd903e369d311ecf47cb2a0e7757ba7e63374a8
user <brezent12345@gmail.com>
2022-07-07 00:21:23 +0300

    two

--- veb/main/views.py
+++ veb/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(requst):
-    return HttpResponse('<h1>First page</h1>')
+    return render(requst, 'main/index.html')
 
 def second(requst):
-    return HttpResponse('<h1>&#128386</h1>')
+    return render(requst, 'main/second.html')
