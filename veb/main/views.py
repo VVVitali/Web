e98d3616f1ea95a6011da126bc017d3b933cc3e4
user <brezent12345@gmail.com>
2022-07-12 23:27:54 +0300

    b

--- veb/main/views.py
+++ veb/main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 def index(requst):
-    return render(requst, 'main/index.html')
+    task = Task.objects.all()
+    return render(requst, 'main/index.html',{'title':'Главная страница', 'tasks': task, 'author': 'author'})
 
 def second(requst):
     return render(requst, 'main/second.html')
