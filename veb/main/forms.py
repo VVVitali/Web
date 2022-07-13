fd25100a4929a44aa5c77a2ff8d35a5998edc0f6
user <brezent12345@gmail.com>
2022-07-14 01:02:37 +0300

    c

--- veb/main/forms.py
+++ veb/main/forms.py
@@ -1,9 +1,13 @@
 from .models import Task
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets, TextInput
 
 
 class Taskform(ModelForm):
     class Meta:
         model = Task
         fields = ["title", "task", "author"]
+        widgets = {"title": TextInput(attrs={"class": "form-control", "placeholder": "Введите название"}),
+                   "task": TextInput(attrs={"class": "form-control", "placeholder": "Введите описание"}),
+                   "author": TextInput(attrs={"class": "form-control", "placeholder": "Введите имя"}),
+                   }
 
