aba06b98c4f45b1367d4fde8ba74e5e756d95b0f
user <brezent12345@gmail.com>
2022-07-14 01:41:32 +0300

    v

--- veb/main/views.py
+++ veb/main/views.py
@@ -13,6 +13,11 @@ def second(requst):
     return render(requst, 'main/second.html')
 
 def create(requst):
+    if requst.method == "POST":
+        form = Taskform(requst.POST)
+        if form.is_valid():
+            form.save()
+
     form = Taskform()
     context = {'form': form}
     return render(requst, 'main/create.html', context)
