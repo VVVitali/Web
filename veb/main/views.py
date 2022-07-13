3d813e743c2276266b37c4f8fc74972a7f8c4156
user <brezent12345@gmail.com>
2022-07-14 01:55:54 +0300

    q

--- veb/main/views.py
+++ veb/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Task
 
@@ -13,13 +13,17 @@ def second(requst):
     return render(requst, 'main/second.html')
 
 def create(requst):
-    if requst.method == "POST":
+    error = ''
+    if requst.method == 'POST':
         form = Taskform(requst.POST)
         if form.is_valid():
             form.save()
-
+            return redirect('home')
+        else:
+            error = 'Форма заполнена неверно'
     form = Taskform()
-    context = {'form': form}
+    context = {'form': form,
+               'error': error}
     return render(requst, 'main/create.html', context)
 
 def four(requst):
